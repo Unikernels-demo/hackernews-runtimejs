7eefca180a2b1029a3d0a37f0d2894a2fb62d83f
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2018-11-13 14:21:57 +0100

    prepare for removing fetchPosts (if Alessio is ok with that !)

--- frontend-runtimejs/index.js
+++ frontend-runtimejs/index.js
@@ -1,52 +1,54 @@
-'use strict';
+'use strict'
+
 const fs = require('fs'),
-      HttpResponse = require('eshttp').HttpResponse,
-      HttpServer = require('eshttp').HttpServer,
-      HttpClient = require('eshttp').HttpClient,
-      HttpRequest = require('eshttp').HttpRequest
+    ESHttp = require('eshttp')
 
-const indexHtml = fs.readFileSync(__dirname + './static/index.html', 'utf8')
+const htmlPageContent = fs.readFileSync(__dirname + './static/index.html', 'utf8')
+let posts = ""
 
-const request = new HttpRequest('GET', '/api/posts', { 'x-header': 'value' });
-const client = new HttpClient('10.0.0.42', 80);
-let posts = "";
+fetchPosts()
 
-client.request(request, function(err, response) {
-    let tmp = "";
-
-    if (err !== null) {
-        tmp = "[]"
-        console.log(err)
-    }
-    else {
-        console.log("Connected to the posts database")
-    }
-
-    response.ondata = function(buf) {
-        console.log("got a chunck of size ", buf.length)
-        var result = Object.keys(buf).map(function(key) {
-	  tmp += String.fromCharCode(buf[key]);
-        });
-    }
-
-    response.onend = function() {
-        console.log("got all posts from the database")
-        posts = tmp;
-    }
-})
-
-const server = new HttpServer()
+const server = new ESHttp.HttpServer()
 server.onrequest = request => {
     if (request._parser._path === '/api/posts') {
-        request.respondWith(new HttpResponse(200, { server: 'demo' }, posts))
+        request.respondWith(new ESHttp.HttpResponse(200, { server: 'demo' }, posts))
     }
     else {
-        request.respondWith(new HttpResponse(200, { server: 'demo' }, indexHtml))
+        request.respondWith(new ESHttp.HttpResponse(200, { server: 'demo' }, htmlPageContent))
     }
 }
 
-server.listen(9000)
-
 const kernelVersion = __SYSCALL.version();
 console.log(`RuntimeJS kernel: ${kernelVersion.kernel} v8: ${kernelVersion.v8} ${Date.now()} x86_64`)
+
+server.listen(9000)
 console.log('listening to port 9000')
+
+function fetchPosts() {
+    const client = new ESHttp.HttpClient('192.168.1.102', 8080)
+    const request = new ESHttp.HttpRequest('GET', '/api/posts')
+
+    client.request(request, function (err, response) {
+        let tmp = ""
+
+        if (err !== null) {
+            tmp = "[]"
+            console.log(err)
+        }
+        else {
+            console.log("Connected to the posts database")
+        }
+
+        response.ondata = function (buf) {
+            console.log("got a chunck of size ", buf.length)
+            Object.keys(buf).map(function (key) {
+                tmp += String.fromCharCode(buf[key]);
+            })
+        }
+
+        response.onend = function () {
+            console.log("got all posts from the database")
+            posts = tmp
+        }
+    })
+}
